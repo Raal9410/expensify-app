173a38a72b5fe2c0e231b465115f2944357629d1
Raul Hernandez <raulhernandez@Rauls-MacBook-Pro.local>
2020-06-02 20:02:14 -0500

    redux playground

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import {NavLink} from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => (
-    <header>
-        <h1>Expensify</h1>
-        <NavLink exact to='/' activeClassName='is-active'>Dashboard</NavLink>
-        <NavLink exact to='/create' activeClassName='is-active'>Create</NavLink>
-        <NavLink exact to='/edit' activeClassName='is-active'>Edit</NavLink>
-        <NavLink exact to='/help' activeClassName='is-active'>Help</NavLink>
-    </header>
-)
+  <header>
+    <h1>Expensify</h1>
+    <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
+    <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
+    <NavLink to="/help" activeClassName="is-active">Help</NavLink>
+  </header>
+);
 
 export default Header;
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,30 +1,25 @@
 import React from 'react';
-import {BrowserRouter,
-        Route,
-        Switch,
-    } from 'react-router-dom';
-import Dashboard from '../components/Dashboard';
-import AddExpense from '../components/AddExpense';
-import EditExpense from '../components/EditExpense';
-import Help from '../components/Help';
-import NotFound from '../components/NotFound';
+import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
+import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
+import AddExpensePage from '../components/AddExpensePage';
+import EditExpensePage from '../components/EditExpensePage';
+import HelpPage from '../components/HelpPage';
+import NotFoundPage from '../components/NotFoundPage';
 import Header from '../components/Header';
 
-
-
 const AppRouter = () => (
-    <BrowserRouter>
+  <BrowserRouter>
     <div>
-    <Header />
-    <Switch>
-        <Route exact path='/' component={Dashboard}/>
-        <Route exact path='/create' component={AddExpense}/> 
-        <Route exact path='/edit' component={EditExpense}/> 
-        <Route exact path='/help' component={Help}/>
-        <Route component={NotFound}/>
-    </Switch>
+      <Header />
+      <Switch>
+        <Route path="/" component={ExpenseDashboardPage} exact={true} />
+        <Route path="/create" component={AddExpensePage} />
+        <Route path="/edit/:id" component={EditExpensePage} />
+        <Route path="/help" component={HelpPage} />
+        <Route component={NotFoundPage} />
+      </Switch>
     </div>
-    </BrowserRouter>
-)
+  </BrowserRouter>
+);
 
 export default AppRouter;
