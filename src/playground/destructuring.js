d9644b0554c42a246b8a4d15ab5557de3c284a77
Raul Hernandez <raulhernandez@Rauls-MacBook-Pro.local>
2020-06-03 13:22:04 -0500

    array destructuring

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,3 +1,5 @@
+// OBJECT DESTRUCTURING
+
 // const person = {
 //     name: 'Raul',
 //     age: 26 ,
@@ -14,14 +16,30 @@
 // console.log(`${name} is ${age} years old. And lives in ${city}.`)
 
 
-const book = {
-    title: 'Ego is the enemy',
-    author: 'Ryan Holiday',
-    publisher: {
-        name: 'Penguin'
-    }
-}
+// const book = {
+//     title: 'Ego is the enemy',
+//     author: 'Ryan Holiday',
+//     publisher: {
+//         name: 'Penguin'
+//     }
+// }
 
-const {name: publisherName = 'Self-published'} = book.publisher
+// const {name: publisherName = 'Self-published'} = book.publisher
 
-console.log(publisherName)
+// console.log(publisherName)
+
+
+// ARRAY DESTRUCTURING
+
+// const address = ['1299 S Juniper Street', 'Philadelphia', 'Pennsylvania', '19147']
+
+// const [, city, state,] = address
+
+// console.log(`You are in ${city}, ${state}.`)
+
+const item = ['Coffee (hot)', '$2.00', '$2.50', '$2.75']
+
+const [product, , medium, ] = item
+
+console.log(`A medium ${product} costs ${medium}`)
+
