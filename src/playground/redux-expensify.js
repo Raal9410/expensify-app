52279446c94502f7efbced33421f33d99ed41e28
Raul Hernandez <raulhernandez@Rauls-MacBook-Pro.local>
2020-06-13 13:57:40 -0500

    sorting by amount and date

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -135,6 +135,13 @@ const getVisibleExpenses = (expenses, {text, sortBy, startDate, endDate}) => {
         const textMatch = expense.description.toLowerCase().includes(text.toLowerCase()) 
 
         return startDateMatch && endDateMatch && textMatch
+    }).sort((a, b) => {
+        if(sortBy === 'date'){
+            return a.createdAt < b.createdAt ? 1 : -1
+        }
+         else if(sortBy === 'amount'){
+            return a.amount < b.amount ? 1: -1
+        }
     })
 }
 
@@ -152,17 +159,17 @@ store.subscribe(() => {
     console.log(visbleExpenses)
 })
 
-const expenseOne = store.dispatch(addExpense({description: 'Rent', amount: 100, createdAt: 1000}))
-const expenseTwo = store.dispatch(addExpense({description: 'Coffee', amout: 200, createdAt: -1000}))
+const expenseOne = store.dispatch(addExpense({description: 'Rent', amount: 100, createdAt: -21000}))
+const expenseTwo = store.dispatch(addExpense({description: 'Coffee', amount: 2000, createdAt: -1000}))
 
 // store.dispatch(removeExpense({id: expenseOne.expense.id}))
 
 // store.dispatch(editExpense(expenseTwo.expense.id, {amount: 500}))
 
-store.dispatch(setTextFilter('rent'))
+//store.dispatch(setTextFilter('rent'))
 // store.dispatch(setTextFilter())
 
-// store.dispatch(sortByAmount())
+store.dispatch(sortByAmount())
 // store.dispatch(sortByDate())
 
 //store.dispatch(setStartDate(0))
