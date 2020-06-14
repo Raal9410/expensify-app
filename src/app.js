07101e60dd7ed76ec036fd9d63f16682b3c03b1c
Raul Hernandez <raulhernandez@Rauls-MacBook-Pro.local>
2020-06-14 14:24:42 -0500

    preparing React to connect with Redux

--- src/app.js
+++ src/app.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore'
+import {addExpense} from './actions/expenses'
+import {setTextFilter} from './actions/filters'
+import getVisibleExpenses from './selectors/expenses'
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+
+const store = configureStore()
+
+store.dispatch(addExpense({description: 'Water bill', amount: 100, createdAt: -1000}))
+store.dispatch(addExpense({description: 'Gas bill', amount: 100, createdAt: -1000}))
+
+store.dispatch(setTextFilter('gas'))
+const state = store.getState()
+const visbleExpenses = getVisibleExpenses(state.expenses, state.filters)
+
+
+console.log(visbleExpenses)
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
