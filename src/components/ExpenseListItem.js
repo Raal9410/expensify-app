d7ca06f0429707d972594a15e9a409bab832e84c
Raul Hernandez <raulhernandez@Rauls-MacBook-Pro.local>
2020-06-14 16:16:55 -0500

    Remove and filter

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -2,10 +2,17 @@
 // description, amount, createdAt
 
 import React from 'react'
+import {connect} from 'react-redux'
+import { removeExpense } from '../actions/expenses'
 
-export default ({description, amount, createdAt}) => (
+const ExpenseListItem = ({description, amount, createdAt, id, dispatch}) => (
     <div>
         <h3>{description}</h3>
         <p>{amount} - {createdAt}</p>
+        <button onClick={() => {
+            dispatch(removeExpense({id}))
+        }}>Remove</button>
     </div>
 )
+
+export default connect()(ExpenseListItem)
