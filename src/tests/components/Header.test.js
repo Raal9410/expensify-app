01d7a8921ec8b52e850bb774b12749171f11779c
Raul Hernandez <raulhernandez@Rauls-MacBook-Pro.local>
2020-06-15 23:51:20 -0500

    enzyme config

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,11 +1,10 @@
 //react-test-renderer
 
 import React from 'react'
-import ReactShallowRenderer from 'react-test-renderer/shallow'
+import {shallow} from 'enzyme'
+
 import Header from '../../components/Header'
 
 test('should render header correctly', () => {
-    const renderer = new ReactShallowRenderer()
-    renderer.render(<Header />)
-    expect(renderer.getRenderOutput()).toMatchSnapshot()
+    const wrapper = shallow(<Header/>)
 })
